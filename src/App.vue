c5e7f58da566e9bbd4ebafda69f1fdad20d46012
jsj <tjdwn1171@gmail.com>
2026-07-18 11:58:42 +0900

    자식 컴포넌트로 데이터 보여주기, 부모 컴포넌트로 이벤트 전파 후 처리 과정 학습

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,36 @@
     {{ message }}
     <HiPage :localId="localId"></HiPage>
     <UserListPage></UserListPage>
+    <MemoDraftLesson
+      v-model="memoContent"
+      
+      @clear-memo="clearMemo"
+    />
   </div>
 </template>
 
 <script>
 import HiPage from './components/HiPage.vue';
 import UserListPage from './components/UserListPage.vue';
-
+import MemoDraftLesson from './views/MemoDraftLesson.vue';
 
 export default {
 
   components : {
     HiPage,
     UserListPage,
+    MemoDraftLesson,
+
   },
 
   data() {
     return {
       message : "MESSAGE !!!!",
       localId : "52676",
+      memoContent : "",
     }
   },
+
   beforeCreate() {
     console.log(`beforeCreate`);
   },
@@ -38,6 +47,16 @@ export default {
   updated() {
     console.log(`updated`);
   },
+  methods : {
+    clearMemo() {
+      this.memoContent = "";
+      console.log("부모에서 이벤트 전파 확인 후 clearMemo메서드 실행");
+    },
+    changeMemo(newValue) {
+      this.memoContent = newValue;
+      console.log(newValue);
+    }
+  }
     
 }
 </script>
---
+++ src/views/MemoDraftLesson.vue
@@ -0,0 +1,72 @@
+/**
+ * 메모 입력창
+ * → 사용자가 입력
+ * → Vue 데이터 변경
+ * →  아래 미리보기 갱신
+ */
+
+ <template>
+  <div>
+    <h1>메모 작성</h1>
+
+    <label for="memoContent">
+      메모 내용
+    </label>
+
+    <input
+        id="memoContent"
+        :value="value"
+        @input="changeMemo"
+        type="text"
+        placeholder="메모를 입력하세요"
+    >
+
+    <p>
+      입력 중인 메모: {{ memoContent }}
+    </p>
+
+    <button
+      type="button"
+      @click="clearMemo"
+    >
+      메모 지우기
+    </button>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemoDraftLesson',
+
+//   model: {
+    // prop: 'memoContent',
+    // event: 'change-memo'
+//   },
+
+  props: {
+    memoContent: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+  },
+
+  methods: {
+    changeMemo(event) {
+        this.$emit("input",event.target.value);
+        // this.$emit("change-memo",event.target.value);
+
+    },
+    clearMemo(event) {
+      this.$emit(
+        'clear-memo',
+        event.target.value
+      );
+      console.log("자식에서 이벤트 전파 함수 실행 ");
+    }
+  },
+};
+</script>
